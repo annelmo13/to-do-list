16dab3ac768fe706716c09abc49ede1416ee1b45
anna <annelmo13@gmail.com>
2021-05-15 20:25:24 +0300

    Add inputField styles, try to do button

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,29 +1,75 @@
-<template>git commit -m "Add placeholder"
+<template>
 <div class="wrapper">
 <header>Tasks</header>
 <div class="inputField">
 <input type="text" placeholder="Add task">
+<button><font-awesome-icon :icon="['fas', 'faPlus']" /></button>
 </div>
+<ul class="todoList">
+</ul>
 </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
-
 export default defineComponent({
   name: "HelloWorld",
   props: {
   
   },
 });
+
+import { library } from '@fortawesome/fontawesome-svg-core'
+
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
 
-header{
-font-weight: bold;
-color:#2c3e50;
-font-family: Avenir, Helvetica, Arial, sans-serif;
+.wrapper{
+  background: #fff;
+  max-width: 400px;
+  width: 100%;
+  margin: 120px auto;
+  padding: 25px;
+  border-radius: 5px;
+  box-shadow: 0px 10px 15px rgba(0,0,0,0.1);
 }
+.wrapper header{
+ font-size: 20px;
+ font-weight: bold;
+ color:#2c3e50;
+ font-family: Arial, Helvetica, sans-serif;
+}
+.wrapper .inputField{
+  margin: 20px 0;
+  width: 100%;
+  display: flex;
+  height: 45px;
+}
+.inputField input{
+  width: 85%;
+  height: 100%;
+  outline: none;
+  border-radius: 3px;
+  border: 1px solid #ccc;
+  font-size: 17px;
+  padding-left: 15px;
+  transition: all 0.3s ease;
+}
+.inputField input:focus{
+  border-color: #2c3e50;;
+}
+
+.inputField button{
+  width: 50px;
+  height: 100%;
+  border: none;
+  color:honeydew;
+  margin-left: 10px;
+  cursor: pointer;
+  border-radius: 3px;
+  pointer-events: none;
+}
+
 </style>
--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,12 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+ 
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faPlus } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
-createApp(App).use(store).use(router).mount("#app");
+library.add(faPlus);
+
+
+createApp(App).use(store).use(router).component("font-awesome-icon", FontAwesomeIcon).mount("#app");
